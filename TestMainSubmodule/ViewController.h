14458323c05ad4661a6805d41ccae28a3b0be3ea
吳佳真 Chia-Chen Wu <d006@kfsyscc.org>
2014-11-12 10:26:54 +0800

    test init

---
+++ TestMainSubmodule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMainSubmodule
+//
+//  Created by 吳佳真 Chia-Chen Wu on 2014/11/12.
+//  Copyright (c) 2014年 吳佳真 Chia-Chen Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
